175ae68e0a295a0d29778be032bdf627b73ed488
Kundan Singh <kundan.singh979@gmail.com>
2018-07-27 14:40:22 +0530

    [feat]: submit feedback page design

--- src/components/StudentFeedbackForm/StudentFeedbackForm.js
+++ src/components/StudentFeedbackForm/StudentFeedbackForm.js
@@ -3,6 +3,9 @@ import { Form, Field } from 'react-final-form';
 import StarRatingComponent from 'react-star-rating-component';
 
 import {
+  Container,
+  Row,
+  Col,
   Button,
   FormGroup,
   Label,
@@ -46,100 +49,205 @@ class StudentFeedbackForm extends React.Component {
   render() {
     const { response, form, subtopicsArray } = this.state;
     return (
-      <div>
-        {response}
-        <h1>
-          Feedback Submission
-        </h1>
-        <h2>
-          Subject -
-          { form.subject }
-        </h2>
-        <h2>
-          Topic -
-          { form.topic }
-        </h2>
-        <Form
-          onSubmit={this.handleSubmit}
-          render={({
-            handleSubmit, submitting, pristine, invalid,
-          }) => (
-            <form name="feedbackSubmission" onSubmit={handleSubmit}>
-              <h2>
-                Subtopics -
-              </h2>
-              {
-                subtopicsArray.map((subtopic, i) => {
-                  return (
-                    <FormGroup key={subtopic}>
-                      <Field name={`rating${i}`} validate={validations.required}>
-                        {({ input, meta }) => (
-                          <div>
-                            <Label for="rating">
-                              {subtopic}
-                            </Label>
-                            <br />
-                            <StarRatingComponent
-                              {...input}
-                              name="rating"
-                              id="rating"
-                              onStarClick={input.onChange}
-                              starCount={10}
-                            />
-                            <FormError meta={meta} />
-                          </div>
-                        )}
-                      </Field>
-                    </FormGroup>
-                  );
-                })
-              }
-              <FormGroup>
-                <Field name="comment" validate={validations.required}>
-                  {({ input, meta }) => (
-                    <div>
-                      <Label for="Comment">
-                        Comment
-                      </Label>
-                      <br />
-                      <Input
-                        {...input}
-                        type="textarea"
-                        name="comment"
-                        id="comment"
-                        className="form-control mt-2"
-                      />
-                      <FormError meta={meta} />
-                    </div>
-                  )}
-                </Field>
-              </FormGroup>
-              <FormGroup>
-                <Field name="revisit">
-                  {({ input, meta }) => (
-                    <div>
-                      <Label for="revisit">
-                        revisit
-                      </Label>
-                      <Input
-                        {...input}
-                        type="checkbox"
-                        name="revisit"
-                        id="revisit"
-                      />
-                      <FormError meta={meta} />
-                    </div>
-                  )}
-                </Field>
-              </FormGroup>
-              <Button type="submit" disabled={submitting || pristine || invalid} color="primary" size="lg" block>
-                submit
-              </Button>
-            </form>
+      <Container>
+        <Row>
+          <Col
+            sm="12"
+            md="12"
+            lg="12"
+            xl="12"
+            className="mx-auto text-center"
+          >
+            <h2>
+              Feedback Submission
+            </h2>
+          </Col>
+        </Row>
+        <Row>
+          {response}
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <h4>
+              {'Subject - '}
+              <span className="text-info">
+                { form.subject }
+              </span>
+            </h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <h4>
+              {'Topic - '}
+              <span className="text-info">
+                { form.topic }
+              </span>
+            </h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form
+              onSubmit={this.handleSubmit}
+              render={({
+                handleSubmit, submitting, pristine, invalid,
+              }) => (
+                <form name="feedbackSubmission" onSubmit={handleSubmit}>
+                  <h4>
+                    Subtopics:
+                  </h4>
+                  {
+                    subtopicsArray.map((subtopic, i) => {
+                      return (
+                        <FormGroup key={subtopic}>
+                          <Field
+                            name={`rating${i}`}
+                            initialValues={{ value: null }}
+                            validate={validations.required}
+                          >
+                            {({ input, meta }) => (
+                              <div>
 
-          )}
-        />
-      </div>
+                                <Row>
+                                  <Col
+                                    sm="3"
+                                    md="3"
+                                    lg="3"
+                                    xl="3"
+                                  >
+                                    <Label for="rating">
+                                      {subtopic}
+                                    </Label>
+                                  </Col>
+                                  <Col>
+                                    <StarRatingComponent
+                                      {...input}
+                                      name="rating"
+                                      id="rating"
+                                      onStarClick={input.onChange}
+                                      starCount={10}
+                                    />
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <FormError meta={meta} />
+                                  </Col>
+                                </Row>
+                              </div>
+                            )}
+                          </Field>
+                        </FormGroup>
+                      );
+                    })
+                }
+                  <Row>
+                    <Col>
+                      <FormGroup>
+                        <Field name="comment" validate={validations.required}>
+                          {({ input, meta }) => (
+                            <Row>
+                              <Col
+                                sm="3"
+                                md="3"
+                                lg="3"
+                                xl="3"
+                              >
+                                <Label for="Comment">
+                                  Comment
+                                </Label>
+                              </Col>
+                              <Col
+                                sm="3"
+                                md="3"
+                                lg="3"
+                                xl="3"
+                              >
+                                <Input
+                                  {...input}
+                                  type="textarea"
+                                  name="comment"
+                                  id="comment"
+                                  className="form-control mt-2"
+                                />
+                              </Col>
+                              <FormError meta={meta} />
+                            </Row>
+                          )}
+                        </Field>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <FormGroup>
+                        <Field type="checkbox" name="revisit">
+                          {({ input, meta }) => (
+                            <div>
+                              <Row>
+                                <Col
+                                  sm="3"
+                                  md="3"
+                                  lg="3"
+                                  xl="3"
+                                >
+                                  <Label for="revisit">
+                                    Revisit
+                                  </Label>
+                                </Col>
+                                <Col
+                                  sm="3"
+                                  md="3"
+                                  lg="3"
+                                  xl="3"
+                                >
+                                  <Input
+                                    {...input}
+                                    type="checkbox"
+                                    name="revisit"
+                                    id="revisit"
+                                  />
+                                </Col>
+                              </Row>
+                              <FormError meta={meta} />
+                            </div>
+                          )}
+                        </Field>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col
+                      md="4"
+                      lg="2"
+                      xl="2"
+                      className="mx-auto"
+                    >
+                      <Button
+                        type="submit"
+                        disabled={submitting || pristine || invalid}
+                        color="primary"
+                        block
+                      >
+                        Submit
+                      </Button>
+                    </Col>
+                    <Col
+                      md="4"
+                      lg="2"
+                      xl="2"
+                      className="mx-auto"
+                    >
+                      {' '}
+                    </Col>
+                  </Row>
+                </form>
+              )}
+            />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }

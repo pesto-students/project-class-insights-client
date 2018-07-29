fe9ec4f4a1a5a4ef5cfcbe1aa44b287c8b59182a
Kundan Singh <kundan.singh979@gmail.com>
2018-07-30 03:39:54 +0530

    [feat]: edit batches modal

--- src/components/BatchesPage/BatchesPage.js
+++ src/components/BatchesPage/BatchesPage.js
@@ -1,23 +1,20 @@
 /* eslint no-underscore-dangle: 0 */
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
+
 import {
   Container,
   Row,
   Col,
   Button,
-  Modal,
-  ModalBody,
-  ModalHeader,
-  ModalFooter,
-  Form,
-  Input,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
-
-import { FRONTEND_URL, BACKEND_URL } from '../../constants/auth.constant';
+import { NavLink } from 'react-router-dom';
+import { BACKEND_URL } from '../../constants/auth.constant';
 
 import { defaultOptions } from '../../helpers/auth-header';
+
+import BatchesModal from './modalComponent';
 
 class BatchesPage extends Component {
   constructor(props) {
@@ -34,6 +31,7 @@ class BatchesPage extends Component {
     };
     this.manageBatch = this.manageBatch.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
+    this.updateBatchInfo = this.updateBatchInfo.bind(this);
   }
 
   async componentWillMount() {
@@ -48,7 +46,7 @@ class BatchesPage extends Component {
         students: val.studentCount,
         startDate: (new Date(val.from)).toISOString().split('T')[0],
         endDate: (new Date(val.to)).toISOString().split('T')[0],
-        details: `${FRONTEND_URL}/batchdetails?batchId=${val._id}&batchName=${val.batchId}`,
+        details: `/batchdetails?batchId=${val._id}&batchName=${val.batchId}`,
       };
       return remappedValues;
     });
@@ -59,6 +57,10 @@ class BatchesPage extends Component {
     this.setState(previousState => ({
       modal: !previousState.modal,
     }));
+  }
+
+  updateBatchInfo() {
+    this.toggleModal();
   }
 
   manageBatch(batch) {
@@ -128,14 +130,9 @@ class BatchesPage extends Component {
         accessor: 'details',
         Cell: row => (
           <Row className="align-items-center">
-            <Button
-              type="button"
-              href={row.value}
-              size="sm"
-              className="mx-auto"
-            >
-                Batch Details
-            </Button>
+            <NavLink to={row.value} className=" mx-auto btn btn-secondary btn-sm">
+              Batch Details
+            </NavLink>
           </Row>
         ),
       },
@@ -167,6 +164,7 @@ class BatchesPage extends Component {
         <BatchesModal
           isOpen={modal}
           toggleModalFunction={this.toggleModal}
+          submitModalFunction={this.updateBatchInfo}
           className={className}
           batchDetails={selectedBatch}
         />
@@ -174,7 +172,8 @@ class BatchesPage extends Component {
         <Button
           type="button"
           className="btn btn-primary btn-block"
-          href="/add"
+          block
+          to="/add"
         >
           Add Batch
         </Button>
@@ -189,104 +188,12 @@ const ActionHeader = () => (
   </span>
 );
 
-const BatchesModal = (props) => {
-  const {
-    isOpen,
-    toggleModalFunction,
-    className,
-    batchDetails,
-  } = props;
-  return (
-    <Modal isOpen={isOpen} toggle={toggleModalFunction} className={className}>
-      <Form>
-        <ModalHeader toggle={toggleModalFunction}>
-          <Row>
-            <Col>
-              Batch Information
-            </Col>
-          </Row>
-        </ModalHeader>
-        <ModalBody>
-          <Row>
-            <Col>
-              {'Batch Name'}
-            </Col>
-            <Col>
-              <Input type="text" defaultValue={batchDetails.batchID} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {'Status'}
-            </Col>
-            <Col>
-              <Input type="text" defaultValue={batchDetails.status} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {'Students'}
-            </Col>
-            <Col>
-              <Input type="text" defaultValue={batchDetails.students} disabled />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {'Start Date'}
-            </Col>
-            <Col>
-              <Input type="date" defaultValue={batchDetails.startDate} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {'End Date'}
-            </Col>
-            <Col>
-              <Input type="date" defaultValue={batchDetails.endDate} />
-            </Col>
-          </Row>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" type="Submit" onClick={toggleModalFunction}>
-            Save changes
-          </Button>
-          <Button color="secondary" onClick={toggleModalFunction}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Form>
-    </Modal>
-  );
-};
-
 BatchesPage.defaultProps = {
   className: null,
 };
 
 BatchesPage.propTypes = {
   className: PropTypes.string,
-};
-
-BatchesModal.defaultProps = {
-  isOpen: false,
-  toggleModalFunction: null,
-  UpdateBatchFunction: null,
-  className: '',
-  batchDetails: {},
-};
-
-BatchesModal.propTypes = {
-  isOpen: PropTypes.bool,
-  toggleModalFunction: PropTypes.func,
-  className: PropTypes.string,
-  batchDetails: PropTypes.shape({
-    batchID: PropTypes.string,
-    status: PropTypes.number,
-    students: PropTypes.number,
-    startDate: PropTypes.string,
-  }),
 };
 
 export default BatchesPage;
